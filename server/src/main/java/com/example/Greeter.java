6859098b633ad1d66e8334f58de511ffb1a1b1c5
Johnsanjay <johnu4693@gmail.com>
2022-06-02 11:56:48 -0500

    Adding bye bye

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,4 +16,8 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
+public String create(String someone) {
+    return String.format("Bye Bye, %s!", someone);
+  }
 }
+
